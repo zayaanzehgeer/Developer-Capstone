c9be11610a10e7a4b34f9a7e6c9758b1821e4c0e
zayaanzehgeer <zayaanzehgeer.gmail.com>
2023-12-14 23:33:22 +0530

    Just another commit

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
